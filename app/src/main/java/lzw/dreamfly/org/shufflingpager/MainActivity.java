af436073db3c74f7a531bbca2fd54b47400db362
luckzhiwei <lzwuestc@sina.com>
2015-11-12 16:36:56 +0800

    写了一个很简单的自动轮播

--- app/src/main/java/lzw/dreamfly/org/shufflingpager/MainActivity.java
+++ app/src/main/java/lzw/dreamfly/org/shufflingpager/MainActivity.java
@@ -1,43 +1,60 @@
 package lzw.dreamfly.org.shufflingpager;
 
+import android.app.Activity;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     private ViewPager mViewPager;
-
+    private Button btn;
+    private int mIndex;
+    private int mViewCount;
+    private ShufftingThread mShufftingThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+    }
+
+    private void init(){
+             mViewPager=(ViewPager)this.findViewById(R.id.viewpager_mainactivity_shuffling);
+             List<View> view=new ArrayList<>();
+             for(int i=0;i<3;i++) {
+              view.add(LayoutInflater.from(this).inflate(R.layout.one_pager_layout, null));
+             }
+             this.mIndex=0;
+             this.mViewCount=3;
+             MyPagerAdapter pagerAdapter=new MyPagerAdapter(view);
+             this.mViewPager.setAdapter(pagerAdapter);
+             this.mShufftingThread=new ShufftingThread(mHandler);
+             this.mShufftingThread.start();
+             this.mViewPager.setCurrentItem(0,true);
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private Handler mHandler=new Handler(Looper.getMainLooper()){
+        public void handleMessage(Message msg){
+            super.handleMessage(msg);
+            mIndex=(mIndex+1)%mViewCount;
+            mViewPager.setCurrentItem(mIndex,true);
         }
-
-        return super.onOptionsItemSelected(item);
-    }
+    };
 }
--- app/src/main/java/lzw/dreamfly/org/shufflingpager/MyPagerAdapter.java
+++ app/src/main/java/lzw/dreamfly/org/shufflingpager/MyPagerAdapter.java
@@ -3,26 +3,47 @@ package lzw.dreamfly.org.shufflingpager;
 import android.support.v4.view.PagerAdapter;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import java.util.List;
 
 /**
  * Created by asus on 2015/11/11.
  */
 public class MyPagerAdapter extends PagerAdapter {
 
+    private List<View> mListViews;
+    private int mViewCount;
+
+    public MyPagerAdapter(List<View> mListViews){
+          this.mListViews=mListViews;
+          this.mViewCount=this.mListViews.size();
+    }
 
     @Override
     public void destroyItem(ViewGroup container, int position, Object object)   {
+          container.removeView(this.mListViews.get(position));
     }
 
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {  //这个方法用来实例化页卡
-        return null;
+        View pager=this.mListViews.get(position);
+        LinearLayout picLayout=(LinearLayout)pager.findViewById(R.id.page_layout);
+        if(position==0){
+              picLayout.setBackgroundResource(R.drawable.pic_one);
+        }else if(position==1){
+              picLayout.setBackgroundResource(R.drawable.pic_two);
+        }else if(position==2) {
+            picLayout.setBackgroundResource(R.drawable.pic_three);
+        }
+        container.addView(pager);
+        return (mListViews.get(position));
     }
 
     @Override
     public int getCount() {
-        return  0;
+        return  (this.mViewCount);
     }
 
     @Override
